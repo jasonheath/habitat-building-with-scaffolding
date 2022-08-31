b09ce87863e3feff23c27937ff01d04d878e78b9
Jason Heath <jh@jasonheath.com>
2022-08-31 08:21:33 -0400

    changed pkg_deps to the imagemagick I built

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -2,5 +2,5 @@ pkg_name=meme-machine
 pkg_origin=jah_tryhabitat
 pkg_version="0.2.0"
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_deps=( core/imagemagick )
+pkg_deps=( jah_tryhabitat/imagemagick )
 
